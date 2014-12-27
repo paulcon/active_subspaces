dcbb332eb303d4605a41401001128c1dd4afd0a2
Paul Constantine <paul.g.constantine@gmail.com>
2014-12-27 12:58:13 -0700

    filling out test scripts

--- test_scripts.py
+++ test_scripts.py
@@ -21,10 +21,35 @@ def quad_fun_nograd(x):
     return f
         
 def test_interface(fun):
+    M,m = 300,3
+    X = np.random.uniform(-1.0,1.0,(M,m))
+    F,dF = ac.sample_function(X,fun,dflag=True)
+    
+    e,W,e_br,sub_br = ac.compute_active_subspace(dF,2)
+    
+    ac.plot_eigenvalues(e,e_br=e_br)
+    ac.plot_subspace_errors(sub_br)
+    ac.plot_eigenvectors(W[:,:1])
+    
+    # make 1d sufficient summary plots
+    y = np.dot(X,W[:,0])
+    ac.sufficient_summary_plot(y,F)
+    
+    # make 2d sufficient summary plots
+    y = np.dot(X,W[:,:2])
+    ac.sufficient_summary_plot(y,F)    
+    
+    return 0
+    
+def test_interface_nograd(fun):
     MM,M,m = 30,300,3
     X = np.random.uniform(-1.0,1.0,(M,m))
     F = ac.sample_function(X,fun,dflag=False)
+
+    # test finite difference gradients
+    ddF = ac.finite_difference_gradients(X,fun)
     
+    # test local linear model gradients
     XX = np.random.uniform(-1.0,1.0,(MM,m))
     ddF = ac.local_linear_gradients(X,F,XX)
     
@@ -37,50 +62,103 @@ def test_interface(fun):
     # make 1d sufficient summary plots
     y = np.dot(X,W[:,0])
     ac.sufficient_summary_plot(y,F)
-    
+
+    # make 2d sufficient summary plots
+    y = np.dot(X,W[:,:2])
+    ac.sufficient_summary_plot(y,F)    
+
     return 0
 
 def test_load_data():
 
     # load data
-    df = pn.DataFrame.from_csv('HyShotII.txt')
+    df = pn.DataFrame.from_csv('quad_nograd.txt')
     data = df.values
-    X = data[:,0:7]
-    F = data[:,7]
-    M = F.shape[0]
-    m = X.shape[1]
+    X = data[:,0:3]
+    F = data[:,3]
+    M,m = X.shape
     
-    # normalize inputs
-    xl = np.array([2.6,0.1,122.5,1.6448e7,3.0551e6,0.6*0.05,0.6*0.145])
-    xu = np.array([4.6,1.9,367.5,1.9012e7,3.4280e6,1.4*0.05,1.4*0.145])
-    XX = ac.normalize_uniform(X,xl,xu)
+    # test local linear model gradients
+    MM = 100
+    XX = np.random.uniform(-1.0,1.0,(MM,m))
+    ddF = ac.local_linear_gradients(X,F,XX)
     
-    # run linear model check
-    # TODO: add check to make sure that there are enough rows in XX and F
-    w,w_boot = ac.linear_model_check(XX,F[:,0])
-     
-    # sample from local linear gradients
-    # TODO add check for number of rows
-    XXX = np.random.uniform(-1.0,1.0,size=(100,m))
-    G = ac.local_linear_gradients(XX,F[:,0],XXX)
+    e,W,e_br,sub_br = ac.compute_active_subspace(ddF,2)
     
-    # get active subspace with gradients from local linear grads
-    lam,W,lam_br,sub_br = ac.get_active_subspace(G,3)
+    ac.plot_eigenvalues(e,e_br=e_br)
+    ac.plot_subspace_errors(sub_br)
+    ac.plot_eigenvectors(W[:,:1])
     
     # make 1d sufficient summary plots
-    y = np.dot(XX,W[:,:2])
-    ac.sufficient_summary_plot(y,F[:,0],W[:,:2],w_boot=w_boot)
+    y = np.dot(X,W[:,0])
+    ac.sufficient_summary_plot(y,F)
+
+    # make 2d sufficient summary plots
+    y = np.dot(X,W[:,:2])
+    ac.sufficient_summary_plot(y,F) 
     
-    # make sufficient summary plots
-    ac.plot_active_subspace(lam,W,lam_br=lam_br,sub_br=sub_br)
+    # load data
+    df = pn.DataFrame.from_csv('quad.txt')
+    data = df.values
+    X = data[:,:3]
+    F = data[:,3]
+    dF = data[:,4:]
+    M,m = X.shape
+    
+    e,W,e_br,sub_br = ac.compute_active_subspace(dF,2)
+    
+    ac.plot_eigenvalues(e,e_br=e_br)
+    ac.plot_subspace_errors(sub_br)
+    ac.plot_eigenvectors(W[:,:1])
+    
+    # make 1d sufficient summary plots
+    y = np.dot(X,W[:,0])
+    ac.sufficient_summary_plot(y,F)
+
+    # make 2d sufficient summary plots
+    y = np.dot(X,W[:,:2])
+    ac.sufficient_summary_plot(y,F) 
     
     return 0
+
+def write_quad_csv():
+    X = np.random.uniform(-1.0,1.0,(100,3))
+    D = np.zeros((100,4))
+    for i in range(100):
+        x = X[i,:]
+        f = quad_fun_nograd(x.T)
+        D[i,:3] = x.T
+        D[i,3] = f
+        
+    labels = ['p1','p2','p3','output']
+    df = pn.DataFrame(data=D,columns=labels)
+    df.to_csv('quad_nograd.txt')
+    
+    X = np.random.uniform(-1.0,1.0,(100,3))
+    D = np.zeros((100,7))
+    for i in range(100):
+        x = X[i,:]
+        f,df = quad_fun(x.T)
+        D[i,:3] = x.T
+        D[i,3] = f
+        D[i,4:] = df.T
+        
+    labels = ['p1','p2','p3','output','d1','d2','d3']
+    df = pn.DataFrame(data=D,columns=labels)
+    df.to_csv('quad.txt')
 
 
 if __name__ == "__main__":
     
-    #if not test_load_data():
-    #    print 'Success!'
-    
-    if not test_interface(quad_fun_nograd):
+    if not test_load_data():
         print 'Success!'
+    
+    if not test_interface(quad_fun):
+        print 'Success!'
+    
+    if not test_interface_nograd(quad_fun_nograd):
+        print 'Success!'
+    
+    
+
+    
