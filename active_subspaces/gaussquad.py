c6693765a9d577077625a83bf49687f6b74a1839
Les Fletcher <les.fletcher@gmail.com>
2015-02-10 13:30:06 -0700

    some code styles updates

--- active_subspaces/gaussquad.py
+++ active_subspaces/gaussquad.py
@@ -14,16 +14,16 @@ def r_hermite(N):
     if type(N) is not int:
         raise TypeError('N must be an int')
 
-    if N<=0:
+    if N <= 0:
         raise ValueError('Parameters out of range.')
 
-    if N==1:
-        return np.array([[0.0,1.0]])
+    if N == 1:
+        return np.array([[0.0, 1.0]])
     else:
-         n = np.array(range(1,N+1))
-         B = np.vstack((1.0,0.5*n.reshape((N,1))))
+         n = np.array(range(1, N + 1))
+         B = np.vstack((1.0, 0.5 * n.reshape((N, 1))))
          A = np.zeros(B.shape)
-         return np.hstack((A,B))
+         return np.hstack((A, B))
 
 def jacobi_matrix(ab):
     """
@@ -44,11 +44,11 @@ def jacobi_matrix(ab):
     if ab.shape[1] != 2:
         raise ValueError('ab must have two columns')
 
-    n = ab.shape[0]-1
-    if n==0:
+    n = ab.shape[0] - 1
+    if n == 0:
         return ab[0,0]
     else:
-        J = np.zeros((n,n))
+        J = np.zeros((n, n))
         J[0,0] = ab[0,0]
         J[0,1] = np.sqrt(ab[1,1])
         for i in range(1,n-1):
@@ -71,17 +71,17 @@ def gh1d(N):
             Numpy array (Nx1), weights
     """
 
-    if N>1:
+    if N > 1:
         ab = r_hermite(N)
         J = jacobi_matrix(ab)
-        e,V = np.linalg.eig(J)
+        e, V = np.linalg.eig(J)
         ind = np.argsort(e)
         x = e[ind].reshape((N,1))
-        x[np.fabs(x)<1e-12] = 0.0
+        x[np.fabs(x) < 1e-12] = 0.0
         w = (V[0,ind]*V[0,ind]).reshape((N,1))
     else:
-        x,w = np.array([[0.0]]),np.array([[1.0]])
-    return x,w
+        x, w = np.array([[0.0]]),np.array([[1.0]])
+    return x, w
 
 def gauss_hermite(N):
     """
@@ -99,21 +99,21 @@ def gauss_hermite(N):
         # TODO: convert to list
 
     if type(N) is int:
-        N = [ N ]
+        N = [N]
 
     if type(N) is not list:
         raise TypeError('N must be a list, yo.')
 
-    if len(N)==1:
+    if len(N) == 1:
         return gh1d(N[0])
     else:
         x = np.array([[1.0]])
         w = np.array([[1.0]])
         for n in N:
-            xi,wi = gh1d(n)
+            xi, wi = gh1d(n)
 
-            xL = np.kron(x.copy(),np.ones(xi.shape))
-            xU = np.kron(np.ones((x.shape[0],1)),xi)
-            x = np.hstack((xL,xU))
-            w = np.kron(w.copy(),wi)
-        return x[:,1:],w
+            xL = np.kron(x.copy(), np.ones(xi.shape))
+            xU = np.kron(np.ones((x.shape[0],1)), xi)
+            x = np.hstack((xL, xU))
+            w = np.kron(w.copy(), wi)
+        return x[:,1:], w
