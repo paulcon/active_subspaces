tests and fixing gradients

active_subspaces/tests/test_gradients.py
@@ -3,6 +3,7 @@ import unittest
 import helper
 import numpy as np
 import active_subspaces.gradients as gr
+from active_subspaces.utils.simrunners import SimulationRunner
 
 class TestGradients(TestCase):
 
@@ -26,9 +27,18 @@ class TestGradients(TestCase):
         data = helper.load_test_npz('test_llm_gradients.npz')
         np.testing.assert_equal(df, data['df'])        
 
-    @unittest.skip('not implemented yet')
     def test_finite_difference_gradients(self):
-        self.fail('not implemented yet')
+        def myfun(x):
+            return 2 - 5*x[0,0] + 4*x[0,1]
+            
+        sr = SimulationRunner(myfun)
+        data = helper.load_test_npz('train_points_10_2.npz')
+        X = data['X'].copy()
+
+        df = gr.finite_difference_gradients(X, sr)
+        M = df.shape[0]
+        df_test = np.tile(np.array([-5.0, 4.0]), (M,1))
+        np.testing.assert_array_almost_equal(df, df_test, decimal=6)
 
 if __name__ == '__main__':
     unittest.main()

active_subspaces/utils/simrunners.py
@@ -28,10 +28,10 @@ class SimulationRunner():
         # should be parallelized
 
         X, M, m = process_inputs(X)
-        F = np.zeros((M, 1))
+        f = np.zeros((M, 1))
         for i in range(M):
-            F[i] = self.fun(X[i,:].reshape((1,m)))
-        return F
+            f[i] = self.fun(X[i,:].reshape((1,m)))
+        return f
 
 class SimulationGradientRunner():
     """